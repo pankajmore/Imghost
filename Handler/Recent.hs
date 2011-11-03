9d4fb07e8d0358267780da0c9950471c422b6248
satvikc <mystic.satvik@gmail.com>
2011-11-03 21:50:20 +0530

    Recent.hs refactored

--- Handler/Recent.hs
+++ Handler/Recent.hs
@@ -8,13 +8,10 @@ import Models
 import Control.Applicative
 import Helpers.Document
 getRecentR :: Handler RepHtml
-getRecentR = do
-                imgList <- map getPair <$> runDB (selectList [] [Desc ImagesCreated, LimitTo 10])
-                defaultLayout $ do 
+getRecentR = defaultLayout $ do 
                                 setTitle "Recent Images"
                                 addStylesheet $ StaticR css_slider_min_css
                                 addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"
                                 addScript $ StaticR js_slider_js
                                 addScript $ StaticR js_demo_js
                                 $(widgetFile "recent")
-        where getPair i = (fst i , getThumb (imagesImageName $ snd i)) 
