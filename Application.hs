fedb9c13fad1671566148db4e6ee0a2c69c2a2dc
satvikc <mystic.satvik@gmail.com>
2011-11-05 00:26:52 +0530

    Upload using curl now supported

--- Application.hs
+++ Application.hs
@@ -14,6 +14,7 @@ import Handler.Image
 import Handler.Vote
 import Handler.Download
 import Handler.Delete
+import Handler.Api
 import Handler.Profile
 import Handler.Json
 import Yesod.Auth
---
+++ Handler/Api.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies, QuasiQuotes, MultiParamTypeClasses,TemplateHaskell, OverloadedStrings #-}
+module Handler.Api (getApiR,postApiR) where
+import Foundation
+import Forms.Upload
+import Helpers.Document
+import Helpers.Storage
+import qualified Data.Text as T
+import System.Cmd (system)
+import Data.Time (getCurrentTime)
+import qualified Data.ByteString.Lazy as L
+postApiR :: Handler ()
+postApiR = do
+    content <- runRequestBody
+    let maybeTag = lookup "tag"$ fst content 
+    case maybeTag of 
+        Just t -> if (t,t) `elem` defaultTags 
+            then do 
+                let fileInfo = snd.head.snd $ content 
+                let extension = getExtension $ fileName fileInfo
+                if T.isPrefixOf "image" (fileContentType fileInfo) 
+                    then do 
+                        randName <- getRandomName extension
+                        time <- liftIO getCurrentTime
+                        mid <- maybeAuthId
+                        let image = Image { name = randName
+                                          , tag = t  
+                                          , owner = mid 
+                                          , caption = getName $ fileName fileInfo
+                                          , votes = 0 
+                                          , created = time
+                                          }
+                        liftIO $ L.writeFile (T.unpack $ T.append uploadDirectory randName) $ fileContent fileInfo
+                        liftIO $ system . T.unpack  $ T.concat ["convert ",uploadDirectory,randName," -thumbnail 100x100^ -gravity center -extent 100x100 ",uploadDirectory,getThumb randName]
+                        id <- storeImagePersist image
+                        return ()
+                    else liftIO $ print ("Not an image" ++ T.unpack (fileContentType fileInfo))
+            else liftIO $ print "Not a default tag given"
+        _ -> liftIO $ print "tag not given"
+    return ()
+
+getApiR = postApiR
+
+
