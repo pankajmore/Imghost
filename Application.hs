d20e3c880a78dc464101fcf19a92edf563391bb1
Pankaj More <pankajmore@gmail.com>
2011-11-04 10:31:51 +0530

    Changing the static settings and db settings to confirm to scafffold site for ease of deployment

--- Application.hs
+++ Application.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE TypeFamilies, QuasiQuotes, MultiParamTypeClasses,TemplateHaskell, OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE CPP #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Application where
 import Foundation
 import Helpers.Document
-import Network.Wai.Application.Static (defaultWebAppSettings)
 import Database.Persist.Sqlite
 import Handler.Root
 import Handler.Upload
@@ -26,15 +27,26 @@ import Yesod.Default.Main
 import Yesod.Default.Handlers
 import Yesod.Logger (Logger)
 import Data.ByteString (ByteString)
+import Settings
+import qualified Database.Persist.Base
+import Database.Persist.GenericSql (runMigration)
+
 
 mkYesodDispatch "ImgHost" resourcesImgHost
 
 withImgHost :: AppConfig DefaultEnv -> Logger -> (Application -> IO ()) -> IO ()
 withImgHost conf logger f = do
-    withSqlitePool "test.db3" openConnectionCount $ \pool -> do
-        runSqlPool (runMigration migrateAll) pool
-        runSqlPool (runMigration migrateComments) pool
-        let h = ImgHost conf logger (Static defaultWebAppSettings) pool
+#ifdef PRODUCTION
+    s <- static Settings.staticDir
+#else
+    s <- staticDevel Settings.staticDir
+#endif
+    dbconf <- withYamlEnvironment "config/sqlite.yml" (appEnv conf)
+            $ either error return . Database.Persist.Base.loadConfig
+    Database.Persist.Base.withPool (dbconf :: Settings.PersistConfig) $ \p -> do
+        Database.Persist.Base.runPool dbconf (runMigration migrateAll) p
+        Database.Persist.Base.runPool dbconf (runMigration migrateComments) p
+        let h = ImgHost conf logger s p
         defaultRunner f h
 
 
